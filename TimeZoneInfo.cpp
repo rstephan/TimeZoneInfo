c80523985968c9416a2051db07aa525c388371ea
Stephan Ruloff <stephan@rk-tech.org>
2016-04-26 13:45:27 +0200

    #pragma pack is important. Added Timezone-name and isDst

--- TimeZoneInfo.cpp
+++ TimeZoneInfo.cpp
@@ -7,7 +7,7 @@ You can't calculate it! This is why linux uses a database to make DST work.
 So, why not using it for your Arduino?
 It is a bit "heavy", prox. 4kB for the database of one location + the code to make it work.
 
-@warning This is a 32-Bit Version! Year 2038 will come fast!
+@warning This is a 32-Bit version! Year 2038 will come fast!
 
 @author Stephan Ruloff
 @date 24.04.2016
@@ -17,18 +17,20 @@ It is a bit "heavy", prox. 4kB for the database of one location + the code to ma
 #include "TimeZoneInfo.h"
 
 
-typedef struct
+TimeZoneInfo::TimeZoneInfo()
 {
-  uint32_t tt_gmtoff;
-  int8_t tt_isdst;
-  uint8_t tt_abbrind;
-} ttinfo;
-
+  mTzFile = 0;
+  mCharPos = 0;
+  mCharLen = 0;
+  memset(&mTimeInfo, 0, sizeof(mTimeInfo));
+}
 
 void TimeZoneInfo::setLocation_P(const byte* tzfile)
 {
   mTzFile = (byte*)tzfile;
-  findTimeInfo(1477789201);
+  mCharPos = 0;
+  mCharLen = 0;
+  memset(&mTimeInfo, 0, sizeof(mTimeInfo));
 }
 
 int32_t TimeZoneInfo::utc2local(int32_t utc)
@@ -47,6 +49,34 @@ int32_t TimeZoneInfo::local2utc(int32_t local)
   offs = findTimeInfo(local);
 
   return local - offs;
+}
+
+// e.g. CEST/CET
+String TimeZoneInfo::getShortName(void)
+{
+  uint32_t pos = mTimeInfo.tt_abbrind;
+  String s;
+  uint8_t c;
+  uint8_t nullCnt = 0;
+
+  if (mCharPos) {
+    while (pos < mCharLen) {
+      c = read8(mCharPos + pos);
+      if (c == 0) {
+        break;
+      } else {
+        s += (char)c;
+      }
+      pos++;
+    }
+  }
+
+  return s;
+}
+
+boolean TimeZoneInfo::isDst(void)
+{
+  return mTimeInfo.tt_isdst != 0;
 }
 
 uint32_t TimeZoneInfo::read32(unsigned long pos)
@@ -83,8 +113,8 @@ int32_t TimeZoneInfo::findTimeInfo(int32_t t)
   uint32_t valTime;
   uint32_t valTimeOld;
   uint32_t valIndex;
-  uint32_t gmtoff = 0;
   boolean found = false;
+  unsigned long pos;
   
   magic = read32(0);
   if (magic != 0x545a6966) {
@@ -97,33 +127,27 @@ int32_t TimeZoneInfo::findTimeInfo(int32_t t)
   typecnt = read32(36);
   charcnt = read32(40);
   
-  //Serial.println(gmtcnt);
-  //Serial.println(timecnt);
-  //Serial.println(typecnt);
-  
   valTimeOld = read32(44);
   for (i = 1; i < timecnt; i++) {
     valTime = read32(44 + (i * 4));
-    //Serial.println(i);
     if (valTimeOld <= t && t < valTime) {
       found = true;
       i--;
-      Serial.print("i=");
-      Serial.println(i);
       break;
     }
     valTimeOld = valTime;
   }
   if (found) {
     valIndex = read8(44L + (timecnt * 4) + i);
-    Serial.println(valIndex);
     if (valIndex < typecnt) {
-      gmtoff = read32(44L + (timecnt * 4) + timecnt + (sizeof(ttinfo) * valIndex));
-      Serial.println(gmtoff);
+      pos = 44L + (timecnt * 4) + timecnt + (sizeof(ttinfo) * valIndex);
+      mTimeInfo.tt_gmtoff = read32(pos);
+      mTimeInfo.tt_isdst = read8(pos + 4);
+      mTimeInfo.tt_abbrind = read8(pos + 5);
+      mCharPos = 44L + (timecnt * 5) + (sizeof(ttinfo) * typecnt) + (8 * leapcnt);
+      mCharLen = charcnt;
     }
-  } else {
-    Serial.println("Not found??");
   }
   
-  return gmtoff;
+  return mTimeInfo.tt_gmtoff;
 }
--- TimeZoneInfo.h
+++ TimeZoneInfo.h
@@ -3,19 +3,32 @@
 
 #include <Arduino.h>
 
+#pragma pack(push, 1)
+typedef struct
+{
+  int32_t tt_gmtoff;
+  int8_t tt_isdst;
+  uint8_t tt_abbrind;
+} ttinfo;
+#pragma pack(pop)
+
 class TimeZoneInfo
 {
 public:
+  TimeZoneInfo();
   void setLocation_P(const byte* tzfile);
   int32_t utc2local(int32_t utc);
   int32_t local2utc(int32_t local);
-  String getShortName(int32_t t); // e.g. CEST/CET
-  boolean isDst(int32_t t);
+  String getShortName(void); // e.g. CEST/CET
+  boolean isDst(void);
 private:
   int32_t findTimeInfo(int32_t t);
   uint32_t read32(unsigned long pos);
   uint8_t read8(unsigned long pos);
   byte* mTzFile;
+  ttinfo mTimeInfo;
+  unsigned long mCharPos;
+  uint32_t mCharLen;
 };
 
 #endif
